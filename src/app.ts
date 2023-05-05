ba97920216340a3d64dbcf4c84b34e3bbb07d1a5
LucasZuq <lucasdeozuquetto@hotmail.com>
2023-05-05 14:05:54 -0300

    feat: criação de diferentes .env para testes e para desenvolvimento

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import { loadEnv } from "./config/envs";
+
+loadEnv()
 
 const app = express();
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import app from "./app"
-import dotenv from "dotenv"
+import app from "./app";
 
-dotenv.config()
+const port = process.env.PORT || 4000;
 
-const port = process.env.PORT || 4000
-
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => {
+   console.log(`${process.env.NODE_ENV} environment`);
+   console.log(`Server is listening on port ${port}`);
+});
