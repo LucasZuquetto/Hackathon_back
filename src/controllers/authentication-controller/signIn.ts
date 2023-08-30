8db1972139099927dc806cb8fab8269c372cd013
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2023-08-30 15:23:14 -0300

    fix jwt import

--- src/controllers/authentication-controller/signIn.ts
+++ src/controllers/authentication-controller/signIn.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export async function signInController(req: Request, res: Response) {
    if (!process.env.JWT_SECRET) {
