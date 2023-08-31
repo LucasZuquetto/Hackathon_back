41e3293fbabdbfe1ac6ba8c4e308f48bbf6d2b89
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2023-08-31 17:54:38 -0300

    create product model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,8 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.ts";
 
-export const User = sequelize.define(
-   "users",
-   {
-      name: DataTypes.TEXT,
-      password: DataTypes.TEXT,
-      email: DataTypes.TEXT,
-   }
-);
+export const User = sequelize.define("users", {
+   name: DataTypes.TEXT,
+   password: DataTypes.TEXT,
+   email: DataTypes.TEXT,
+});
