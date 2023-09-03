a66cae20d0c8febc510bac9d4cd99f88b99a2479
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2023-09-02 23:09:29 -0300

    create error response to sequelize validation err

--- src/controllers/api-controller.ts
+++ src/controllers/api-controller.ts
@@ -13,11 +13,15 @@ export async function readController(req: Request, res: Response) {
 
 export async function createController(req: Request, res: Response) {
    const { tableName } = req.params;
-   const data = req.body
+   const data = req.body;
    try {
-      const createdObject = await createService(tableName,data);
+      const createdObject = await createService(tableName, data);
       res.send(createdObject);
-   } catch (error) {
-      res.sendStatus(404);
+   } catch (error: any) {
+      if(error.name = "SequelizeValidationError"){
+         return res.status(400).send(error.errors.map((error: any) => error.message));
+      }
+      console.log(error.errors.map((error:any) => error.path))
+      res.status(404).send(error.name);
    }
 }
